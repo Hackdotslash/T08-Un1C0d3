68d9efa23002e78e66c76d7f8856900f4d42cae1
rajkharvar <rajkharvar1808@gmail.com>
2021-01-31 00:07:55 +0530

    folder restructure and refactoring for base

--- base/app.js
+++ base/app.js
@@ -2,14 +2,17 @@
 let focusedClassifier, suspiciousClassifier, identityClassifier;
 // Model URL
 let focusedModelUrl =
-  "https://teachablemachine.withgoogle.com/models/rpglqcptK/";
+  "https://teachablemachine.withgoogle.com/models/lPaUnGWj_/";
 let suspiciousModelUrl =
-  "https://teachablemachine.withgoogle.com/models/2R4sgrR0w/";
+  "https://teachablemachine.withgoogle.com/models/6ZSm6dKX9/";
 let identityModelUrl =
-  "https://teachablemachine.withgoogle.com/models/I7IVFVhGV/";
+  "https://teachablemachine.withgoogle.com/models/jlXMvG6DG/";
 
 let video;
 let flippedVideo;
+let label = "Checking";
+let label1 = "Checking";
+let label2 = "Checking";
 
 // * Load all the models
 function preload() {
@@ -19,16 +22,21 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(320, 260);
+  createCanvas(520, 460);
   // Create the video
   video = createCapture(VIDEO);
-  video.size(320, 240);
+  video.size(400, 320);
   video.hide();
 
   flippedVideo = ml5.flipImage(video);
-  // Start classifying
+  // Trigger all Classifier at first
+  initAllClassifier();
+}
+
+function initAllClassifier() {
   classifyFocused();
   classifySuspicious();
+  classifyIdentity();
 }
 
 function draw() {
@@ -40,7 +48,9 @@ function draw() {
   fill(255);
   textSize(16);
   textAlign(CENTER);
-  // text(label, width / 2, height - 4);
+  text(label, width / 2, height - 50);
+  text(label1, width / 2, height - 30);
+  text(label2, width / 2, height - 10);
 }
 
 // * For focused
@@ -62,7 +72,7 @@ function focusedResult(error, results) {
   }
   // The results are in an array ordered by confidence.
   // console.log(results[0]);
-  console.log(results[0].label);
+  label = results[0].label;
   // Classifiy again!
 }
 
@@ -85,16 +95,16 @@ function suspiciousResult(error, results) {
   }
   // The results are in an array ordered by confidence.
   // console.log(results[0]);
-  console.log(results[0].label);
+  label1 = results[0].label;
   // Classifiy again!
 }
 
 // * For identity mode
 setInterval(() => {
-  identitySuspicious();
+  classifyIdentity();
 }, 100);
 
-function identitySuspicious() {
+function classifyIdentity() {
   flippedVideo = ml5.flipImage(video);
   identityClassifier.classify(flippedVideo, identityResult);
   // flippedVideo.remove();
@@ -108,6 +118,7 @@ function identityResult(error, results) {
   }
   // The results are in an array ordered by confidence.
   // console.log(results[0]);
-  console.log(results[0].label);
+  label2 = results[0].label;
+
   // Classifiy again!
 }
